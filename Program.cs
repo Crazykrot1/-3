70d65fa87a19b0fc09b9c19d93848f606bb1855a
Kirill <kirill.rayov@mail.ru>
2022-10-12 19:55:53 +0500

    ДЗ 3

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿//Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+/*Console.WriteLine("Введите Координаты");
+int xa = int.Parse(Console.ReadLine()!);
+int ya = int.Parse(Console.ReadLine()!);
+int za = int.Parse(Console.ReadLine()!);
+int xb = int.Parse(Console.ReadLine()!);
+int yb = int.Parse(Console.ReadLine()!);
+int zb = int.Parse(Console.ReadLine()!);
+//Math.sqrt - квадратный корень
+double result =Math.Sqrt(Math.Pow((xb-xa),2)+Math.Pow((yb-ya),2)+Math.Pow((zb-za),2));
+Console.WriteLine($"{result}");
+*/
+//Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+/*Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()!);
+int i =1;
+int resul=0;
+while(i<=a){
+    resul =Convert.ToInt32(Math.Pow(i,3));
+    Console.WriteLine($"{resul}");
+    i++;}
+    */
+    //Задача 19  
+    //Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. (можно решить через строку, а можно математически)
+   
+   /* 
+   Console.WriteLine("Введите число");
+   string a = Console.ReadLine()!;
+
+if(a.Length ==5){ 
+     if(a[0] == a[4] && a[1] == a[3]){
+        Console.WriteLine(" число палиндром");
+     }
+     else{
+        Console.WriteLine(" число не палиндром");
+     }
+    
+} 
+
+else
+{
+  Console.WriteLine(" неверное число");  
+} */
